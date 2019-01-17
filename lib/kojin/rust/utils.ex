4d9a6791975ac64b83e6215c1d68e332708db360
Daniel Davidson <dbdavidson@yahoo.com>
2019-01-16 20:53:45 -0600

    unified `triple_slash_comment` and `script_comment` -- added tests

--- lib/kojin/rust/utils.ex
+++ lib/kojin/rust/utils.ex
@@ -3,20 +3,38 @@ defmodule Kojin.Rust.Utils do
   Rust utility functions.
   """
 
-  @commentLineTrailingWhite ~r"///\s+\n"
-  @commentFinalTrailingWhite ~r"///\s+$"
-
-  def triple_slash_comment(text) do
-    indent = "  "
-
+  def comment(text, opener) do
     result =
       text
-      |> String.replace(~r/(?:\r|\n)*$/, "")
+      |> String.replace(~r/(?:\r|\n)+$/, "")
       |> String.split("\n")
-      |> Enum.join("\n///#{indent}")
-      |> String.replace(@commentLineTrailingWhite, "///\n")
-      |> String.replace(@commentFinalTrailingWhite, "///")
+      |> Enum.join("\n#{opener}")
 
-    "///#{indent}#{result}\n"
+    "#{opener}#{result}\n"
   end
+
+  def triple_slash_comment(text) do
+    comment(text, "///  ")
+  end
+
+  def script_comment(text) do
+    comment(text, "#  ")
+  end
+
+  def indent_block(text, options \\ [])
+
+  def indent_block(text, _) when text == nil do
+    nil
+  end
+  
+  def indent_block(text, options) do
+    defaults = [indent: "  "]
+    %{indent: indent} = Keyword.merge(defaults, options) |> Enum.into(%{})
+
+    text
+    |> String.split("\n")
+    |> Enum.map(fn line -> "#{indent}#{line}" end)
+    |> Enum.join("\n")
+  end
+
 end
