1a4eedab836a8cde4dbae57178e07e1344f1bd16
Daniel Davidson <dbdavidson@yahoo.com>
2019-11-23 10:21:20 -0600

    added test files for object and package

---
+++ test/pod/pod_object_test.exs
@@ -0,0 +1,23 @@
+defmodule PodObjectTest do
+  use ExUnit.Case
+  alias Kojin.Pod.PodObject
+  alias Kojin.Pod.PodTypes
+
+  import PodSamples
+
+  test "all_types" do
+    assert(
+      [:int64, :string, :vaccination]
+      |> Enum.map(fn t -> PodTypes.pod_type(t) end)
+      |> MapSet.new() == PodObject.all_types(sample_object())
+    )
+  end
+
+  test "all_ref_types" do
+    assert(
+      [:vaccination]
+      |> Enum.map(fn t -> PodTypes.pod_type(t) end)
+      |> MapSet.new() == PodObject.all_ref_types(sample_object())
+    )
+  end
+end
---
+++ test/pod/pod_package_test.exs
@@ -0,0 +1,15 @@
+defmodule PodPackageTest do
+  use ExUnit.Case
+  alias Kojin.Pod.PodPackage
+  alias Kojin.Pod.PodTypes
+
+  import PodSamples
+
+  test "all_types" do
+    assert(
+      [:int64, :string, :vaccination]
+      |> Enum.map(fn t -> {:pet_store, PodTypes.pod_type(t)} end)
+      |> MapSet.new() == PodPackage.all_types(sample_package())
+    )
+  end
+end
